8fabfdbc0108777989d9204a25606384fe41421d
Ignacio Catalán <110937506+Gitnacio@users.noreply.github.com>
2026-06-16 16:42:26 -0400

    Added +10 and -10 buttons

--- CounterLogic.js
+++ CounterLogic.js
@@ -1,5 +1,7 @@
 const subs_button = document.getElementById('subs_life');
 const add_button = document.getElementById('add_life');
+const subs_Xbutton = document.getElementById('subs_Xlife');
+const add_Xbutton = document.getElementById('add_Xlife');
 const life_txt = document.getElementById('life_txt');
 const restart_btn = document.getElementById('restart_btn')
 
@@ -17,6 +19,26 @@ subs_button.addEventListener('click', function(){
 
 add_button.addEventListener('click', function(){
     life_count++;
+    life_txt.textContent = life_count;
+    if (life_count <= 0){
+        life_txt.style.color = "rgb(206, 10, 10)";
+    }else{
+        life_txt.style.color = "";
+    }
+})
+
+subs_Xbutton.addEventListener('click', function(){
+     life_count-= 10;
+     life_txt.textContent = life_count;
+    if (life_count <= 0){
+        life_txt.style.color = "rgb(206, 10, 10)";
+    }else{
+        life_txt.style.color = "";
+    }
+})
+
+add_Xbutton.addEventListener('click', function(){
+    life_count+= 10;
     life_txt.textContent = life_count;
     if (life_count <= 0){
         life_txt.style.color = "rgb(206, 10, 10)";
